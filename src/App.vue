42e651ff3d66bd0b263c3a7337766b84527bfd3a
Brian Nelson <ladofmusi@outlook.com>
2019-10-10 20:58:50 -0600

    first commit to prevent anythin from breaking

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,99 @@
+<!--<template>
+  
+
+<div id="app">
+  <v-app id="">
+    <div>
+      <div class="
+         ">
+        <v-card
+          v-for="n in 3"
+          :key="n"
+          class="pa-2"
+          outlined
+          tile
+        >
+          Flex item {{ n }}
+        </v-card>
+      </div>
+      <div class="d-flex flex-column-reverse">
+        <v-card
+          v-for="n in 3"
+          :key="n"
+          class="pa-2"
+          outlined
+          tile
+        >
+          Flex item {{ n }}
+        </v-card>
+      </div>
+    </div>
+  </v-app>
+</div>
+
+</template>-->
+
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <v-app id="test">
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                <header>
+                    <h1>Server status
+                    </h1>
+                </header>
+            </v-col>
+        </v-row>
+        <hr>
+        <v-row>
+            <v-col cols="6">
+        
+                <v-list>
+                    <v-list-item-content v-for="index in 5" :key="index">
+                      
+                    Server #{{ index }}
+<v-divider :inset="inset"></v-divider>
+
+                    </v-list-item-content>
+                </v-list>
+            </v-col>
+            <v-col cols="6">
+                <p>Server Details are currently not updated</p>
+            </v-col>
+        </v-row>
+        <hr>
+        <v-row>
+            <v-col cols="12">
+                <footer>
+                    <p>All Servers are managed here</p>
+                </footer>
+            </v-col>
+        </v-row>
+    </v-container>
+  </v-app>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld';
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import servers from './components/Servers.vue';
+import ServerDetails from './components/ServerDetails.vue';
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    //HelloWorld,
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-servers': Servers,
+    'app-server-details': ServerDetails,
+  },
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
